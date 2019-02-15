5e371aeb3cae55a1c981c46c8829d99e5ad03886
Blackhawk95 <abhijith.newton@gmail.com>
2019-02-15 16:49:55 +0530

    need moreeee

--- src/demo_algo/book.h
+++ src/demo_algo/book.h
@@ -21,27 +21,70 @@ class Book{
     Book(){}
 
     void newBookInit(long int a,int ageCounter);
+    void newBookInit(entry e);
+
     long int getBookName();
+    long int getBookOldAddress();
+
     void insertEntry(long int a,int ageCounter);
+    void insertEntry(entry e);
+
     int findOldEntry();
+    entry deEntry(); 
+
+    //bools
+    bool isEmpty();
 };
+//newBookInit from bare details
 void Book::newBookInit(long int a,int ageCounter){
     
     book = a;
     ent.push_back({a,ageCounter});
     booksize = 1;
 }
+
+//newBookInit from an entry
+void Book::newBookInit(entry e){
+    
+    book = e.address;
+    ent.push_back(e);
+    booksize = 1;
+}
+
 long int Book::getBookName(){
     return book;
 }
-
+//insert a new entry 
 void Book::insertEntry(long int a,int ageCounter){
     ent.push_back({a,ageCounter}); 
     booksize++;
 }
+//insert a already existing entry
+void Book::insertEntry(entry e){
+    booksize++;
+    ent.push_back(e);
+}
 
 int Book::findOldEntry(){
     return ent.front().age; 
+}
+
+entry Book::deEntry(){
+    booksize--;
+    entry temp = ent.front();
+    ent.pop_front();
+    return temp;
+}
+
+bool Book::isEmpty(){
+    if(ent.empty()){
+        return true;
+    }
+    return false;
+}
+
+long int Book::getBookOldAddress(){
+    return ent.front().address;
 }
 
 #endif
--- src/demo_algo/register.h
+++ src/demo_algo/register.h
@@ -6,14 +6,15 @@
 
 struct track{
     long int address;
-    int8_t flag; // bit [0] is flag for empty,
-               // bit [1] is for dirty
+    int8_t flag; // bit 1 in flag is 0 for empty,
+                 // bit 2 in flag is 1 for dirty
 };
 
 class Register{
     std::vector <Book*> reg;
     //8bit address or mini-address(ma) is given by register and stored in each entry of
     //book with it.
+    Book discarded;
     std::array<track,64> trac{};
     int regsize=0;
     int ageCounter = 1;
@@ -34,18 +35,23 @@ int  Register::insert(long int a){
 
     if(reg.empty()){
         int ma = tracInsert(a);
-        if(ma != -1){
+        if(ma != -1 || ma >= 64){
             createBook(a);
             regsize = 1;
         }
-        else
+        else //happens when there is no free space
         {   
-            createSpaceAndInsert(a);
+            //THIS SHOULDN'T HAPPEN
+            printf("ma == -1 in empty register : THIS SHOULDN'T HAPPEN");
+            exit(0);
         }
-        
         return 0;
     }
-    else{
+    else
+    {
+        createSpaceAndInsert(a); // this moves data to dscarded from a queue
+        int ma = tracInsert(a); // this updates the book and creates space
+        //check way to make sure new data is inserted into any book
         for(auto i : reg){
             long int tempA = i->getBookName();
             //checking which book it belongs to
@@ -76,12 +82,22 @@ void Register::createBook(long int a){
 
 int Register::tracInsert(long int a){
     //search the entire trac and decide where to put the data
-    //int tempcounter = 0;
+   
+    //NEW DATA
     for(int i = 0;i < 64; i++){
         if((trac[i].flag & 0x01) != 1){
             trac[i].address = a;
             trac[i].flag|=0x01;
             return i; // found a space
+        }   
+    }
+    //Check for discarded
+    for(int i = 0;i < 64; i++){
+        if((trac[i].flag & 0x02) != 1){
+            //TODO : CALL WRITEBACK, REMOVE DATA FROM Discarded and PUT new data in queue
+            trac[i].address = a;
+            trac[i].flag&=(0xFD); // inverted of 0x02
+            return i + 64; // found a space
         }   
     }
     //no space found
@@ -145,8 +161,26 @@ void Register::createSpaceAndInsert(long int a){
     Book* choosenone = findBookWithOldestEntry();
 
     if(choosenone){
-        //will call function to update the book 
-        //then will call function to update the 8bit address in track
+        //getting address from choosenone to select the trac entry - NEED A BETTER SOLUTION
+        long int a = choosenone->getBookOldAddress();
+        //remove old stuff from boook with old entry
+        //and push it into discarded queue
+        if(discarded.isEmpty()){
+            discarded.newBookInit(choosenone->deEntry());
+        }
+        else
+        {
+            discarded.insertEntry(choosenone->deEntry());
+        }
+        //now update the bits in track
+        //find the correct entry on trac
+            for(int i = 0;i < 64; i++){
+                if(trac[i].address == a){
+                    //update the d bit on the trac
+                    trac[i].flag|=0x02;
+            }   
+        }
+        
     }
     else{
         printf("ERROR findBookWithOldestEntry() gave NULL\n");
