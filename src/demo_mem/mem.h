545a448a4350d25768953230a33d84c24dbf52f3
Blackhawk95 <abhijith.newton@gmail.com>
2019-03-30 06:55:14 +0530

    memory started

---
+++ src/demo_mem/mem.h
@@ -0,0 +1,16 @@
+//int a[1000];
+
+
+/* Memory module
+ *
+ * flag - 8 bit - 0x0000_0000
+ * LSB 
+ * 0 - read/write
+ * 1 - to dram
+ * 2 - move from nvm to dram and place incoming data 
+ * */
+
+
+void mem(long long int a,long int ma,char flag){
+    
+}
